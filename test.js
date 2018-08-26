bf321d66856cea2fef628ff9a5a82b9ad93a44e9
shunkie <shunkie@qq.com>
2018-08-26 23:55:17 +0800

    a simple http server

--- test.js
+++ test.js
@@ -1,2 +1,10 @@
 const Koa = require('koa')
-console.log('Hello Git')
+const app = new Koa()
+
+app.use(ctx => {
+  ctx.body = 'Hello Git'
+})
+
+app.listen(8080, () => {
+  console.log('server is running...')
+})
