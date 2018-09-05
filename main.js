b3b482f7d1eee785ad39e390e3d804d1156c8d4b
shunkie <shunkie@qq.com>
2018-09-05 22:06:44 +0800

    add content-type

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const Koa = require('koa')
 const app = new Koa()
 
 app.use(ctx => {
-  ctx.body = 'Hello Git'
+  ctx.type = 'text/html'
+  ctx.body = '<h1>Hello Git</h1>'
 })
 
 app.listen(8080, () => {
